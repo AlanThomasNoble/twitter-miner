daa7f959c618ee62b1dc078c09af80214569c3eb
Alan <alantn@umich.edu>
2020-06-18 19:18:41 -0400

    FULL_TEXT_tweets_from_list_users(api) - able to obtain full text for list of users

--- twitter_miner_1.py
+++ twitter_miner_1.py
@@ -13,11 +13,11 @@ def start():
     print("This software will be used to mine Twitter data.")
     print()
     print("(1) User - obtain a set of a given user's tweets using an account's user ID")
-    print("(1) Rev_User - revised function for obtaining a given user's tweets using an account's user ID")
-    print("(2) List - obtain tweets from a list of users")
-    print("(3) Search - obtain tweets from a search query")
-    print("(4) Limits - prints json of current API usage limits")
-    print("(5) Exit - exits software")
+    print("(2) List - quickly retrival of tweets from a list of users (cannot guarentee full text)")
+    print("(3) F_List - obtain full text tweets from a list of users")
+    print("(4) Search - obtain tweets from a search query")
+    print("(5) Limits - prints json of current API usage limits")
+    print("(6) Exit - exits software")
     print()
     data = input("Enter the type of data from the above list that you would like to mine (Ex: User, Exit, etc.): ")
     print()
@@ -114,44 +114,19 @@ def obtain_tweets_from_single_user(api):
         print(f"Tweets Retrieved {numTweets}")
 
 
-# EXPERIMENTAL FUNCTION FOR SINGLE USER
-# Revised function for obtaining a full tweet
-# Max is 200 tweets per user
-def REVISED_obtain_tweets_from_single_user(api):
-    user_id = input("Enter user's id (Ex: _AVPodcast, selfdriving360, etc.): ")
-    print()
-    print("Obtaining user's tweets...")
-    print()
-    print("The following tweets are from this account:", user_id)
-    new_tweets = api.user_timeline(screen_name=user_id, count=200, tweet_mode="extended")
-    count = 0
-    for tweet in new_tweets:
-        count += 1
-        if tweet._json['truncated']:
-            print("Found")
-            exit_program()
-        if not tweet._json['retweeted']:
-            print(count, tweet._json["full_text"])
-        else:
-            print("alan")
-            print(count, tweet._json.retweeted_status.full_text)
-        print()
-
-    print("Number of tweets found: ", count)
-
-
-# Action: outputs tweets from a list of users
+# Action: quickly outputs tweets from a list of users
+# Can occasionally obtain a PARTIAL TEXT tweet but will QUICKLY run
 # The file containing the list of accounts is found in the input folder
 # Requirements
 # 1) Each account id corresponds to a PUBLIC accounts
 # 2) Each account id is spelled correctly
-def obtain_tweets_from_list_users(api):
+def PARTIAL_TEXT_tweets_from_list_users(api):
     print("Obtaining tweets from a list of users...")
     print()
 
     # open the file
     f_ptr = open(f'input/list_of_accounts.txt', 'r')
-    w_ptr = open(f'output/list_of_accounts_output.txt', 'w')
+    w_ptr = open(f'output/PARTIAL_TEXT_list_of_accounts_output.txt', 'w')
 
     # go through each line in the file
     running_count = 1
@@ -171,7 +146,55 @@ def obtain_tweets_from_list_users(api):
 
     f_ptr.close()
     w_ptr.close()
-    print("Tweets can be found in new_output.txt")
+    print("Tweets can be found in PARTIAL_TEXT_list_of_accounts_output.txt")
+
+
+# Action: outputs FULL TEXT tweets from a list of users
+# The file containing the list of accounts is found in the input folder
+# WILL OBTAIN FULL TEXT BUT WILL TAKE A LONG TIME TO RUN
+# Requirements
+# 1) Each account id corresponds to a PUBLIC accounts
+# 2) Each account id is spelled correctly
+def FULL_TEXT_tweets_from_list_users(api):
+    num_tweets = input("Enter the number of tweets you would like per user in the list: ")
+    print()
+    print("Obtaining tweets from a list of users...")
+    print()
+
+    # open the file
+    f_ptr = open(f'input/list_of_accounts.txt', 'r')
+    w_ptr = open(f'output/FULL_TEXT_list_of_accounts_output.txt', 'w')
+
+    # go through each line in the file
+    running_count = 0
+    for line in f_ptr:
+        account = line.rstrip('\n')
+        account_tweets = api.user_timeline(account, count=num_tweets, include_rts=True)
+
+        account_line  = "The following tweets are from this account: " + account + "\n"
+        w_ptr.write(account_line)
+
+        # status.retweeted can be used to see if the text was retweeted
+        for each_tweet in account_tweets:
+            running_count += 1
+            # obtains tweet id
+            tweet_id = each_tweet.id
+            # obtains status using tweet id
+            status = api.get_status(tweet_id, tweet_mode="extended")
+            try:
+                w_ptr.write(str(running_count) + ") retweeted: " + status.retweeted_status.full_text + "\n")
+            except AttributeError:  # Not a Retweet
+                w_ptr.write(str(running_count) + ") " + status.full_text + "\n")
+            # written_tweet = str(running_count) + ") " + status.full_text + "\n\n"
+            # w_ptr.write(written_tweet)
+        # w_ptr.write("\n")
+        w_ptr.write("\n")
+        w_ptr.write("\n")
+
+    f_ptr.close()
+    w_ptr.close()
+    print(running_count, "tweets found.")
+    print("Tweets can be found in FULL_TEXT_list_of_accounts_output.txt")
 
 
 # Action: obtains tweets from a search query and returns list of json objects for each result from query
@@ -231,10 +254,10 @@ def main():
 
     if user_input == "User":
         obtain_tweets_from_single_user(api)
-    elif user_input == "Rev_User":
-        REVISED_obtain_tweets_from_single_user(api)
     elif user_input == "List":
-        obtain_tweets_from_list_users(api)
+        PARTIAL_TEXT_tweets_from_list_users(api)
+    elif user_input == "F_List":
+        FULL_TEXT_tweets_from_list_users(api)
     elif user_input == "Search":
         obtain_tweets_from_search(api)
     elif user_input == "Limits":
