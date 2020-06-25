19b6887a5de3e771ba7ff8effe9c3ce4850471ee
Alan <alantn@umich.edu>
2020-06-25 15:53:35 -0400

    twitter_nlp.py - experimented with textblob inputs

--- twitter_nlp.py
+++ twitter_nlp.py
@@ -1,5 +1,6 @@
 import nltk
 import ssl
+import re
 
 # try:
 #     _create_unverified_https_context = ssl._create_unverified_context
@@ -13,9 +14,16 @@ import ssl
 from textblob import TextBlob, Word, Blobber
 
 def mood_function(tweet_text):
-    text_obj = TextBlob(tweet_text)
+    pat = re.compile(r'[^a-zA-Z ]+')
+    answer = re.sub(pat, '', tweet_text.lower())
+    print(answer)
+    text_obj = TextBlob(answer)
     polarity = text_obj.polarity
     subjectivity = text_obj.subjectivity
+
+    text_obj_2 = TextBlob(tweet_text)
+    polarity_2 = text_obj_2.polarity
+    subjectivity_2 = text_obj_2.subjectivity
 
     # We can determine the thresholds for tweet mood
     mood = ""
@@ -26,4 +34,12 @@ def mood_function(tweet_text):
     else:
         mood = "positive"
 
-    return f"mood: {mood} ({polarity}), subjectivity level: {subjectivity}"
+    mood_1 = ""
+    if polarity_2 < -0.1:
+        mood_1 = "negative"
+    elif polarity_2 >= -0.1 and polarity_2 <= 0.1:
+        mood_1 = "neutral"
+    else:
+        mood_1 = "positive"
+
+    return f"textblob --> mood: {mood} ({polarity}), subjectivity level: {subjectivity}\ntextblob --> mood: {mood_1} ({polarity_2}), subjectivity level: {subjectivity_2}"
