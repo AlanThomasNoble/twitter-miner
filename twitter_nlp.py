f5f98ed6251d927473ab9c7ca0f4ef555f7eb719
Alan <alantn@umich.edu>
2020-06-26 15:15:12 -0400

    added readme_nlp

--- twitter_nlp.py
+++ twitter_nlp.py
@@ -13,9 +13,19 @@ import re
 
 from textblob import TextBlob, Word, Blobber
 
+def cleanTxt(text):
+ text = re.sub('@[A-Za-z0–9]+', '', text) #Removing @mentions
+ text = re.sub('#', '', text) # Removing '#' hash tag
+ text = re.sub('RT[\s]+', '', text) # Removing RT
+ text = re.sub('https?:\/\/\S+', '', text) # Removing hyperlink
+ return text
+
 def mood_function(tweet_text):
-    pat = re.compile(r'[^a-zA-Z ]+')
-    answer = re.sub(pat, '', tweet_text.lower())
+    # pat = re.compile(r'[^a-zA-Z ]+')
+    # answer = re.sub(pat, '', tweet_text.lower())
+    # print(answer)
+
+    answer = cleanTxt(tweet_text)
     print(answer)
     text_obj = TextBlob(answer)
     polarity = text_obj.polarity
@@ -31,15 +41,15 @@ def mood_function(tweet_text):
     else:
         mood = "positive"
 
-    # text_obj_2 = TextBlob(tweet_text)
-    # polarity_2 = text_obj_2.polarity
-    # subjectivity_2 = text_obj_2.subjectivity
-    # mood_1 = ""
-    # if polarity_2 < -0.1:
-    #     mood_1 = "negative"
-    # elif polarity_2 >= -0.1 and polarity_2 <= 0.1:
-    #     mood_1 = "neutral"
-    # else:
-    #     mood_1 = "positive"
+    text_obj_2 = TextBlob(tweet_text)
+    polarity_2 = text_obj_2.polarity
+    subjectivity_2 = text_obj_2.subjectivity
+    mood_1 = ""
+    if polarity_2 < -0.1:
+        mood_1 = "negative"
+    elif polarity_2 >= -0.1 and polarity_2 <= 0.1:
+        mood_1 = "neutral"
+    else:
+        mood_1 = "positive"
 
-    return f"textblob --> mood: {mood} ({polarity}), subjectivity level: {subjectivity}"
+    return f"textblob --> mood: {mood} ({polarity}), subjectivity level: {subjectivity}\ntextblob --> mood: {mood_1} ({polarity_2}), subjectivity level: {subjectivity_2}"
