7f1c60a29c0514f5ed58cfdcf3eb135b27edbfc7
Alan <alantn@umich.edu>
2020-07-02 14:02:49 -0400

    twitter_neural_net_generator - added code to get the data in a usable format

--- README_NLP.txt
+++ README_NLP.txt
@@ -2,35 +2,44 @@ LIBRARY #1: nltk/textblob
     https://www.pluralsight.com/guides/natural-language-processing-extracting-sentiment-from-text-data
     https://medium.com/better-programming/twitter-sentiment-analysis-15d8892c0082 
 
-SETTING UP THE PYTHON ENVIRONMENT AND THE NECESSARY LIBRARIES
-    python3.6 -m venv env
+    SETTING UP THE PYTHON ENVIRONMENT AND THE NECESSARY LIBRARIES
+        python3.6 -m venv env
+        source env/bin/activate
+        which python
+        which python3.6
+        pip install -U nltk
+        pip install -U textblob
+        python3.6 -m textblob.download_corpora
+        /Users/alannoble/Documents/Autonomous-Vehicles-Research/Twitter-NLP/env/bin/python3.6 /Users/alannoble/Documents/Autonomous-Vehicles-Research/Twitter-NLP/twitter_NLP.py
+
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            pass
+        else:
+            ssl._create_default_https_context = _create_unverified_https_context
+        nltk.download()
+
+    Output: 
+        Sentiment(polarity=0.7000000000000001, subjectivity=0.825)
+        
+        Polarity 
+        -> float type
+        -> [-1, 1]
+        -> 1 indicates a highly positive sentiment
+        -> -1 indicates a highly negative sentiment
+
+        Subjectivity
+        -> float type
+        -> [0, 1]
+        -> 1 mostly public opinion/not a factual piece of information
+        -> 0 factual
+
+LIBRARY #2
+Building a neural network using TensorFlow and Keras
+
     source env/bin/activate
-    which python
-    which python3.6
-    pip install -U nltk
-    pip install -U textblob
-    python3.6 -m textblob.download_corpora
-    /Users/alannoble/Documents/Autonomous-Vehicles-Research/Twitter-NLP/env/bin/python3.6 /Users/alannoble/Documents/Autonomous-Vehicles-Research/Twitter-NLP/twitter_NLP.py
-
-    try:
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        pass
-    else:
-        ssl._create_default_https_context = _create_unverified_https_context
-    nltk.download()
-
-Output: 
-    Sentiment(polarity=0.7000000000000001, subjectivity=0.825)
+    pip install numpy
+    pip install keras
     
-    Polarity 
-    -> float type
-    -> [-1, 1]
-    -> 1 indicates a highly positive sentiment
-    -> -1 indicates a highly negative sentiment
 
-    Subjectivity
-    -> float type
-    -> [0, 1]
-    -> 1 mostly public opinion/not a factual piece of information
-    -> 0 factual
--- twitter_neural_net_generator.py
+++ twitter_neural_net_generator.py
@@ -1,8 +1,11 @@
+# File for building a neural network using TensorFlow and Keras, and then using this neural network to classify tweets
 
 # 1) Get our data into a usable format
 # 2) Build our neural net
 # 3) Train it with said data
 # 4) Save the neural net for future use
+
+############################################# GETTING DATA IN USABLE FORMAT ###################################################
 
 import numpy as np
 
@@ -10,7 +13,59 @@ import numpy as np
 training = np.genfromtxt('/Users/alannoble/Documents/Autonomous-Vehicles-Research/Sentiment_Analysis_Dataset.csv', delimiter=',', skip_header=1, usecols=(1, 3), dtype=None)
 # print(len(training)) -> 1578627
 
-# create our training data from the tweets
+# list of all the tweets
 train_x = [x[1] for x in training]
-# index all the sentiment labels
+
+# list of all the respective sentiment labels
 train_y = np.asarray([x[0] for x in training])
+
+import sys
+import json
+import keras
+import keras.preprocessing.text as kpt
+from keras.preprocessing.text import Tokenizer
+
+# only work with the 3000 more popular words from out dataset
+max_words = 3000
+
+# create a new tokenizer
+tokenizer = Tokenizer(num_words = max_words) # turns each text into a sequence of integers in order to apply machine learning
+
+# feed our tweets to the tokenizer
+tokenizer.fit_on_text(train_x) # This method creates the vocabulary dict based on word frequency: cat->1 dog->2
+
+dictionary = tokenizer.word_index # Dictionary of word to ID/index
+
+# moves the dictionary that we just got to a file called nn_dictionary.json in the output folder
+with open(output/nn_dictionary.json, 'w') as dictionary_file:
+    json.dump(dictionary, dictionary_file)
+
+def convert_text_to_index_array(text):
+    # text to word sequence converts text to a list of words. A cat and the hat -> [A, cat, and, the, hat]
+    # returns list with ids/index for all the words
+    return [dictionary[word] for word in kpt.text_to_word_sequence(text)]
+
+# for each tweet, take the text, and convert the text to a list of its ID/Index then add that list to a master list
+allWordIndices = []
+for text in train[x]:
+    indices_for_each_word_in_text_list = convert_text_to_index_array(text)
+    allWordIndices.append(indices_for_each_word_in_text_list)
+
+# cast as an array for future use??
+allWordIndices = np.asarray(allWordIndices)
+
+train_x = tokenizer.sequences_to_matrix(allWordIndices, mode='binary')
+train_y = keras.utils.to_categorical(train_y, 2)
+
+
+##############################################################################################################################
+
+############################################# BUILDING OUT NEURAL NETWORK $###################################################
+
+
+
+
+
+
+
+
