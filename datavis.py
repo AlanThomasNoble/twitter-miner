d8bffd3d18f62004d10ef54618534225c1485e1f
ashhari <ashhari@umich.edu>
2020-07-02 00:32:10 -0400

    datavis.py - Added Visuals Class and some functions (not tested), WIP

--- datavis.py
+++ datavis.py
@@ -1,25 +1,57 @@
 import numpy as np 
 from wordcloud import WordCloud
 import matlablib.pyplot as plt
+import pandas as pd
+import pdb
 
-# import pandas as pd # dataframe
+# DB? ..sql, mongo?...more efficient?
 
-# streamline main()
+class Visuals:
+	df = []
 
-# dict(mood=mood, polarity=polarity,subjectivity=subjectivity)
-    # g.update(f) to connect to dicts together, pass into DataFrame.
+	def wordCloud(data, spec):
+		pdb.set_trace()
+		tweets = ' '.join(t for t in df['Tweets']) # [] vs generator...test, check label
+		wordCloud = WordCloud(width=500,height=300, random_state=21, max_font_size=110).generate(tweets)
+		plt.imshow(wordCloud, interpolation=bilinear)
 
-# Write this as a Class
-def wordCloud(data, spec=''):
-	pass
+	def polSub(data, spec): # Plots Polarity and Subjectivity
+		plt.figure(figsize=(8,6))
+		for in range(o, df.shape[0]):
+			plt.scatter(df['Polarity'][i], df['Subjectivity'], color='Blue') # scatter plot: x axis, y axis
 
-def ngram(data, spec=''):
-	pass
+		plt.title('Sentiment Analysis')
+		plt.xlabel('Polarity')
+		plt.ylabel('Subjectivity')
+		plt.show()
 
-def vis(filename, visualization, spec):
-	# Read in Data to a DataFrame
-	with open(filename,'r') as file:
-		pass # read in data here
-		data = []
-	
-	return visualization(data, spec)
+	def analytics(data,spec):
+		ptweets = df[df.Analysis == 'Negative']
+		ptweets = ptweets['Tweets']
+
+		ntweets = df[df.Analysis == 'Negative'] # ''?
+		ntweets = ntweets['Tweets']
+
+		posPercent = round((ptweets.shape[0] / df.shape[0])*100, 1)
+		negPercent = round((ntweets.shape[0] / df.shape[0])*100, 1)
+
+		print(f'Percent Positive Tweets: {posPercent}%')
+		print(f'Percent Negative Tweets: {posNegative}%')
+
+	def valuecount(data, spec):
+		print(f'Value Counts: {df['Analysis'].value_counts()}')
+
+		# Plot and Visualize the Counts
+		plt.title('Sentiment Analysis')
+		plt.xlabel('Sentiment')
+		plt.ylabel('Counts')
+		df['Analysis'].value_counts().plot(kind='bar')
+		plt.show()
+
+	def vis(filename, visualization, spec=''):
+		# Inits
+		df = pandas.read_csv(file)
+		plt.style.use('fivethirtyeight')
+
+		# Call Visualization
+		return visualization(data, spec)
