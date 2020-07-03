c656d1e6a5d2a14545134b42daec28db40ece41b
Alan <alantn@umich.edu>
2020-07-03 15:45:30 -0400

    cleanText - changed function

--- twitter_nlp.py
+++ twitter_nlp.py
@@ -12,38 +12,14 @@ import re
 
 from textblob import TextBlob #, Word, Blobber
 
-# copied a clean txt function: https://ipullrank.com/step-step-twitter-sentiment-analysis-visualizing-united-airlines-pr-crisis/
-'''
-def cleanTxt(tweet):
-    #Convert to lower case
-    tweet = tweet.lower()
-    #Convert www.* or https?://* to URL
-    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
-    #Convert @username to AT_USER
-    tweet = re.sub('@[^\s]+','AT_USER',tweet)
-    #Remove additional white spaces
-    tweet = re.sub('[\s]+', ' ', tweet)
-    #Replace #word with word
-    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    #trim
-    tweet = tweet.strip('\'"')
-    return tweet  
-'''
-
-# Alternative/Merged Implementation of Clean Text
-'''This implementation entirely removes hashtags and mentions'''
-'''pythex.org'''
-def cleanData(text, cleanEmoticons=False, removeFullHashtag=True, removeFullMention=True):
-    # Removals
-    if removeFullMention:
-        text = re.sub(r'@[A-Za-z0-9]+','',text) # Removes mentions
-    else:
-        pass
-    if removeFullHashtag:
-        text = re.sub(r'#[A-Za-z0-9_]+','',text) # Removes hashtags
-    else:
-        pass
+# part of function: https://ipullrank.com/step-step-twitter-sentiment-analysis-visualizing-united-airlines-pr-crisis/
+def cleanTextNN(text, cleanEmoticons=False):
+    # Conversions
+    text = text.lower() # Convert to lowercase
     
+    # Removals
+    text = re.sub(r'@[A-Za-z0-9]+','',text) # Removes mentions
+    text = re.sub(r'#[A-Za-z0-9_]+','',text) # Removes hashtags
     text = re.sub(r'(https?)://[-a-zA-Z0-9@:%_\+.~#?&//=]*','',text) # Removes hyperlink
 
     # Cleanup
@@ -55,6 +31,31 @@ def cleanData(text, cleanEmoticons=False, removeFullHashtag=True, removeFullMent
 
     return text
 
+# # Alternative/Merged Implementation of Clean Text
+# '''This implementation entirely removes hashtags and mentions'''
+# '''pythex.org'''
+# def cleanData(text, cleanEmoticons=False, removeFullHashtag=True, removeFullMention=True):
+#     # Removals
+#     if removeFullMention:
+#         text = re.sub(r'@[A-Za-z0-9]+','',text) # Removes mentions
+#     else:
+#         pass
+#     if removeFullHashtag:
+#         text = re.sub(r'#[A-Za-z0-9_]+','',text) # Removes hashtags
+#     else:
+#         pass
+    
+#     text = re.sub(r'(https?)://[-a-zA-Z0-9@:%_\+.~#?&//=]*','',text) # Removes hyperlink
+
+#     # Cleanup
+#     text = re.sub('[\s]+',' ',text) # Removes additional white spaces
+#     text = text.strip('\'"').lstrip().rstrip() # Trim (Removes '' and Trailing and Leading Spaces)
+
+#     if cleanEmoticons:
+#         pass    # :\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)
+
+#     return text
+
 # Subjectivity and Polarity
 def getSubjectivity(text):
     return TextBlob(text).sentiment.subjectivity # or t.subjectivity (the call to sentiment is really not necessary)
@@ -64,7 +65,7 @@ def getPolarity(text):
 
 # Mood Function
 def mood_function(tweet_text):
-    text = cleanData(tweet_text) # text_obj = TextBlob(cleanTxt(tweet_text))
+    text = cleanText(tweet_text) # text_obj = TextBlob(cleanTxt(tweet_text))
     polarity = getSubjectivity(text) #text_obj.polarity
     subjectivity = getPolarity(text) #text_obj.subjectivity
 
