c125c748b9809b5ae630240aa9e21f13e624fe39
ashhari <ashhari@umich.edu>
2020-08-08 12:30:53 -0400

    Combined all watson files to output/keywords_output_full_watson.csv

---
+++ merger.py
@@ -0,0 +1,10 @@
+import os
+from glob import glob
+import pandas as pd
+os.chdir("./output/watson")
+extension = 'csv'
+files = [i for i in glob(f'*.{extension}')] # all filenames
+# Combine All Files Into List
+df = pd.concat([pd.read_csv(f) for f in files ])
+os.chdir("../../")
+df.to_csv('output/keywords_output_full_watson.csv', index=False, encoding='utf-8-sig')
