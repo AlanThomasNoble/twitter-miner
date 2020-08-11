df893172a2ab603a022a1c5bede8f57561d93402
ashhari <ashhari@umich.edu>
2020-08-11 03:04:34 -0400

    google doc output, watson ouptut, watson file modified slightly

---
+++ keywords.py
@@ -0,0 +1,22 @@
+#default = list['']
+one = ['autonomous vehicles', 'autonomous driving', 'autonomous cars', 'driverless', 'self driving']
+
+two = ['assisted driving', 'autonomous technology', 'autonomous level', 'intelligent driving systems', 'artificial intelligence cars']
+
+three = ['self driving crash', 'autonomous vehicle crash', 'autonomous parking', 'autonomous robots', 'LIDAR in vehicles']
+
+four = ['autonomous driving', 'self driving bikes', 'self driving law', 'self driving safety', 'computer vision cars']
+
+five = ['self driving car accidents','self driving dangers','self driving death','pedestrian autonomous vehicles','self driving issues']
+
+six = ['human machine interface vehicles', 'autonomous vehicles research', 'unmanned vehicle', 'self driving sensors', 'neural network cars']
+
+seven = ['self driving engineer', 'autonomous vehicle engineer', 'deep learning cars', 'self driving future', 'autonomous vehicle benefits']
+
+eight = ['Tesla self driving', 'Waymo self driving', 'Argo AI self driving', 'Lyft self driving', 'Uber self driving']
+
+nine = ['Baidu self driving', 'Zoox self driving', 'nuTonomy self driving', 'Cruise self driving', 'Enbark self driving']
+
+ten = ['Ford self driving', 'BMW self driving', 'Mercedes self driving', 'Voyage self driving', 'Carmera self driving']
+
+eleven = ['Nuro autonomous robot', 'Pony AI autonomous driving', 'self driving expert']
--- twitter_watson.py
+++ twitter_watson.py
@@ -20,97 +20,103 @@ natural_language_understanding.set_service_url(url)
 import pandas as pd
 import re
 
-def cleanData(text):
-    cleaned = str(text)
-    cleaned = re.sub(r'@[A-Za-z0-9]+', '', cleaned)  # Removes mentions
-    cleaned = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))','',cleaned) # Removes hyperlink  
-    cleaned = re.sub(r'(\bNaN\b)|(\bnan\b)','',cleaned) # Removes NaN values 
-    cleaned = re.sub(r'[\s]+', ' ', cleaned)  # Removes additional white spaces
-    cleaned = cleaned.strip('\'"').lstrip().rstrip() # Trim
-    return cleaned
+# def cleanData(text):
+#     cleaned = str(text)
+#     cleaned = re.sub(r'@[A-Za-z0-9]+', '', cleaned)  # Removes mentions
+#     cleaned = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))','',cleaned) # Removes hyperlink  
+#     cleaned = re.sub(r'(\bNaN\b)|(\bnan\b)','',cleaned) # Removes NaN values 
+#     cleaned = re.sub(r'[\s]+', ' ', cleaned)  # Removes additional white spaces
+#     cleaned = cleaned.strip('\'"').lstrip().rstrip() # Trim
+#     return cleaned
 
-def cleanDataframe(df):
-    total = len(df)
-    print(f"Total Entries: {total}")
-    df.drop_duplicates(subset='tweet id')
-    print(f"***{total - len(df)} Duplicate Entries Removed***")
-    total = len(df)
-    df.dropna(subset=['account status'], inplace=True)
-    print(f"***{total - len(df)} nan Entries Removed***")
-    total = len(df)
-    import twitter_processing as processing
-    df['account status'] = df['account status'].apply(processing.cleanData)
-    df['account status'].replace('',float('nan'),inplace=True)
-    df.dropna(subset=['account status'], inplace=True)
-    print(f"***{total - len(df)} Empty Entries Removed***")
-    print(f"New Total: {len(df)}\n")
-    return df
+# def cleanDataframe(df):
+#     total = len(df)
+#     print(f"Total Entries: {total}")
+#     df.drop_duplicates(subset='tweet id')
+#     print(f"***{total - len(df)} Duplicate Entries Removed***")
+#     total = len(df)
+#     df.dropna(subset=['account status'], inplace=True)
+#     print(f"***{total - len(df)} nan Entries Removed***")
+#     total = len(df)
+#     import twitter_processing as processing
+#     df['account status'] = df['account status'].apply(processing.cleanData)
+#     df['account status'].replace('',float('nan'),inplace=True)
+#     df.dropna(subset=['account status'], inplace=True)
+#     print(f"***{total - len(df)} Empty Entries Removed***")
+#     print(f"New Total: {len(df)}\n")
+#     return df
 
-def analyze(text):
-    print(f'Running Count: {analyze.counter}\r')
-    analyze.counter +=1
-    if text: # Error Occurs if Empty Text is Passed In
-        response = natural_language_understanding.analyze(text=text,language='en', features=Features(emotion=EmotionOptions())).get_result()
-        sadness = response['emotion']['document']['emotion']['sadness']
-        joy = response['emotion']['document']['emotion']['joy']
-        fear = response['emotion']['document']['emotion']['fear']
-        disgust = response['emotion']['document']['emotion']['disgust']
-        anger = response['emotion']['document']['emotion']['anger']
-        scores = [sadness, joy, fear, disgust, anger]
-        names = ['sadness','joy','fear','disgust','anger']
-        group = list(zip(scores,names))
-        tones = list(map(lambda name: name[1], filter(lambda scores: scores[0] > .5, group)))
-        tones = ', '.join(tones)
-        scores.insert(0, tones)
-        return scores
+# def analyze(text):
+#     print(f'Running Count: {analyze.counter}\r')
+#     analyze.counter +=1
+#     if text: # Error Occurs if Empty Text is Passed In
+#         response = natural_language_understanding.analyze(text=text,language='en', features=Features(emotion=EmotionOptions())).get_result()
+#         sadness = response['emotion']['document']['emotion']['sadness']
+#         joy = response['emotion']['document']['emotion']['joy']
+#         fear = response['emotion']['document']['emotion']['fear']
+#         disgust = response['emotion']['document']['emotion']['disgust']
+#         anger = response['emotion']['document']['emotion']['anger']
+#         scores = [sadness, joy, fear, disgust, anger]
+#         names = ['sadness','joy','fear','disgust','anger']
+#         group = list(zip(scores,names))
+#         tones = list(map(lambda name: name[1], filter(lambda scores: scores[0] > .5, group)))
+#         tones = ', '.join(tones)
+#         scores.insert(0, tones)
+#         return scores
 
-    NaN = float('NaN') # workaround
-    return [None,NaN,NaN,NaN,NaN,NaN]
-
-##################################################################################################### 
-'''Read File, Call Functions, Save to CSV File'''
-##################################################################################################### 
-# Read File
-num = 1 #### ****MODIFY HERE***** ####
-
-file = f'keywords_output_{num}'
-df = pd.read_csv(f'output/KSO/{file}.csv')
-
-from twitter_datavis import mkdir
-path = 'output/watson/'
-mkdir(path)
-
-try:
-    # Clean Data
-    df['account status'] = df['account status'].apply(cleanData)
-    df = cleanDataframe(df)
-
-    # Call Tone Analyzer
-    analyze.counter = 1
-    df[['tones', 'sadness_score', 'joy_score','fear_score','disgust_score','anger_score']] = df.apply(
-        lambda row: pd.Series(analyze(row['account status'])), axis=1)
-
-except:# Save to CSV
-    df.to_csv(f'output/watson/{file}_watson.csv')
-
-df.to_csv(f'output/watson/{file}_watson.csv')
+#     NaN = float('NaN') # workaround
+#     return [None,NaN,NaN,NaN,NaN,NaN]
 
 # ##################################################################################################### 
-# '''Entire Document (I think we may be limited by the size of the text input (But hopefully we should be fine)''' 
+# '''Read File, Call Functions, Save to CSV File'''
 # ##################################################################################################### 
-# # Check out Datavis.py
+# # Read File
+# num = 1 #### ****MODIFY HERE***** ####
+
+# file = f'keywords_output_{num}'
+# df = pd.read_csv(f'output/KSO/{file}.csv')
+
+# from twitter_datavis import mkdir
+# path = 'output/watson/'
+# mkdir(path)
+
 # try:
-#     text = ' '.join(df['account status'])
-#     import math
-#     units = math.ceil(len(text)/10000)
-#     print(f'NLU Data Units: {units}')
-#     response = natural_language_understanding.analyze(text=text,language='en', features=Features(emotion=EmotionOptions(targets=['Covid-19','Autonomous Vehicles']))).get_result()
-#     print(response)
-# except:
-#     import json
-#     with open(f'output/watson/entireDocumentOfTweetsResponse_{num}.txt','w') as file:
-#         file.write(json.dumps(response,indent=2))
+#     # Clean Data
+#     df['account status'] = df['account status'].apply(cleanData)
+#     df = cleanDataframe(df)
 
-# import json
-#     with open(f'output/watson/entireDocumentOfTweetsResponse_{num}.txt','w') as file:
-#         file.write(json.dumps(response,indent=2))
+#     # Call Tone Analyzer
+#     analyze.counter = 1
+#     df[['tones', 'sadness_score', 'joy_score','fear_score','disgust_score','anger_score']] = df.apply(
+#         lambda row: pd.Series(analyze(row['account status'])), axis=1)
+
+# except:# Save to CSV
+#     df.to_csv(f'output/watson/{file}_watson.csv')
+
+# df.to_csv(f'output/watson/{file}_watson.csv')
+
+##################################################################################################### 
+'''Entire Document (I think we may be limited by the size of the text input (But hopefully we should be fine)''' 
+##################################################################################################### 
+# Check out Datavis.py
+num = 11
+df = pd.read_csv(f'output/watson/keywords_output_{num}_watson.csv')
+print('Starting Generation...')
+try:
+    text = ' '.join(df['account status'])
+    print(len(text))
+    import math
+    units = math.ceil(len(text)/10000)
+    print(f'NLU Data Units: {units}')
+    response = natural_language_understanding.analyze(text=text,language='en', features=Features(emotion=EmotionOptions(targets=['Nuro autonomous robot', 'Pony AI autonomous driving', 'self driving expert']))).get_result()
+    print(response)
+except:
+    import json
+    with open(f'output/watson/document/entireDocumentOfTweetsResponse_{num}.txt','w') as file:
+        file.write(json.dumps(response,indent=2))
+        file.write(f'\n\n Character Length: {len(text)}')
+
+import json
+with open(f'output/watson/document/entireDocumentOfTweetsResponse_{num}.txt','w') as file:
+    file.write(json.dumps(response,indent=2))
+    file.write(f'\n\n Character Length: {len(text)}')
